2b366fbb15529970293fb74285ffc3ccec7cdd24
Daniel Dolezal <d.dolezal97@protonmail.com>
2021-12-30 01:30:54 +0100

    add builder

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DOCKER_IMAGE_NAME="borgbackup-ssh"
+DOCKER_CONTAINER_NAME="borgbackup"
+
+run_docker_container() {
+  echo "Running..."
+  docker run -dp 3000:22 \
+    -e UID=$(id -u) \
+    -e GID=$(id -g) \
+    -v "$PWD"/sshkeys:/sshkeys \
+    -v "$PWD"/backup:/backup \
+    "$DOCKER_IMAGE_NAME"
+}
+
+build_docker_image() {
+  echo "Building..."
+  docker build -t "$DOCKER_IMAGE_NAME" .
+}
+
+build_docker_image
+run_docker_container
