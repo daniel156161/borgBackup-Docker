10d564dd301c2ac2e97101f1f401b471887685f6
Daniel Dolezal <d.dolezal97@protonmail.com>
2023-04-30 15:40:22 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+source "../build-functions.sh"
+source "../build-config.sh"
 
 DOCKER_IMAGE_NAME="daniel156161/borgbackup-ssh"
 DOCKER_CONTAINER_NAME="borgbackup"
-GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-run_docker_container() {
+function run_docker_container {
   echo "Running..."
   docker run -dp 3000:22 \
     -p 9100:9100 \
@@ -19,39 +20,18 @@ run_docker_container() {
     -v "$PWD"/Testing/test_script.sh:/test_script.sh \
     -v "$PWD"/sshkeys/clients:/sshkeys/clients \
     -v "$PWD"/backups:/backups \
-    "$DOCKER_IMAGE_NAME":"$GIT_BRANCH"
+    "$DOCKER_IMAGE_NAME:$GIT_BRANCH"
 }
-
-build_docker_image() {
-  TAG="$1"
-
-  echo "Building..."
-  docker buildx build --push \
-    --platform linux/amd64 \
-    --tag "$DOCKER_IMAGE_NAME:$TAG" .
-}
-
-if [ "$GIT_BRANCH" == "main" ]; then
-  GIT_BRANCH="latest"
-fi
 
 case "$1" in
   run)
     run_docker_container
     ;;
   build)
-    build_docker_image "$GIT_BRANCH"
-    ;;
-  upload)
-    build_docker_image "$GIT_BRANCH"
-    docker push "$DOCKER_IMAGE_NAME:$GIT_BRANCH"
-    ;;
-  test)
-    build_docker_image "$GIT_BRANCH"
-    run_docker_container
+    build_docker_image "$DOCKER_IMAGE_NAME:$GIT_BRANCH"
     ;;
   *)
-    echo "Usage: $0 {run|build|test|upload}"
+    echo "Usage: $0 {run|build}"
     exit 1
     ;;
 esac
